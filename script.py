a174d26d3ddc11602a493b9babaa5088a57480af
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2023-02-22 18:28:27 -0800

    homework

--- script.py
+++ script.py
@@ -33,3 +33,24 @@ home_town = {
 }
 
 print(f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}")
+
+#excercise 5:
+#Iterate over the key: value pairs in home_townand print a string for each item, for example
+
+for key, value in home_town.items():
+    print(f"The {key} of my honetown is {value}")
+
+
+#excercise 6:
+#Create an empty list named cohort.
+#Using a forloop, add one dictionary to the cohortlist for each student name. Each dictionary should have this shape
+
+
+cohort = []
+student_names = ["Tina", "Jenny", "Sara", "Sally", "Sue"]
+
+for student in student_names:
+    cohort.append({"student": student, "fav_food": "pizza"})
+    print(f"{student}'s favorite food is pizza")
+
+
